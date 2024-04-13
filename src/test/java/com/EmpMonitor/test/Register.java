42ea05d483d6fd372d7ac7ca231b3c2d7ab4ec8e
Naveen22025 <naveenkumarap22025@gmail.com>
2024-04-13 13:39:11 +0530

    first commit

--- src/test/java/com/EmpMonitor/test/Register.java
+++ src/test/java/com/EmpMonitor/test/Register.java
@@ -3,6 +3,7 @@ package com.EmpMonitor.test;
 import org.testng.annotations.Test;
 import org.testng.annotations.Test;
 
+import com.EmpMonitor.Pom.Dashboard;
 import com.EmpMonitor.genrics.BasePropCls;
 
 public class Register extends BasePropCls{
@@ -12,5 +13,9 @@ public class Register extends BasePropCls{
 	public void RegisterName() {
 		
 		System.out.println("Register User");
+		
+		Dashboard d= new Dashboard(driver);
+		d.getEmployee().click();
+		d.getEmpDetails().click();
 	}
 }
